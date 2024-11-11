d64ba78b742c965e217614c96908ff9603ecc4e8
aziz1489 <azizshavkatov333@gmail.com>
2024-11-11 22:42:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    
+        const url = 'https://aziz1489.github.io/shiboya/';
+   
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                window.location.href = url; 
+            }
+        });
